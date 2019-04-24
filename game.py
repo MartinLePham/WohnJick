8311051d8d24ae1f560bb529070839edf1265b6e
ap16080 <47228489+ap16080@users.noreply.github.com>
2019-04-23 23:28:35 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -19,7 +19,13 @@ walkLeft = [pygame.transform.flip(pygame.image.load('images/character_sprites/ad
 jumpRight = [pygame.image.load('images/character_sprites/adventurer-jump-00.png'), pygame.image.load('images/character_sprites/adventurer-jump-01.png'), pygame.image.load('images/character_sprites/adventurer-jump-02.png'), pygame.image.load('images/character_sprites/adventurer-jump-03.png')]
 jumpLeft = [pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-jump-00.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-jump-01.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-jump-02.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-jump-03.png'),True, False)]
 bg = pygame.transform.scale(pygame.image.load('images/background/Background.png'), (SCREEN_WIDTH,SCREEN_HEIGHT))
-char = [pygame.image.load('images/character_sprites/adventurer-idle-2-00.png'), pygame.image.load('images/character_sprites/adventurer-idle-2-01.png'), pygame.image.load('images/character_sprites/adventurer-idle-2-02.png'), pygame.image.load('images/character_sprites/adventurer-idle-2-03.png')]
+idle = [pygame.image.load('images/character_sprites/adventurer-idle-2-00.png'), pygame.image.load('images/character_sprites/adventurer-idle-2-01.png'), pygame.image.load('images/character_sprites/adventurer-idle-2-02.png'), pygame.image.load('images/character_sprites/adventurer-idle-2-03.png')]
+idleLeft = [pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-idle-2-00.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-idle-2-01.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-idle-2-02.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-idle-2-03.png'),True, False)]
+attack =  [pygame.image.load('images/character_sprites/adventurer-attack1-00.png'), pygame.image.load('images/character_sprites/adventurer-attack1-01.png'), pygame.image.load('images/character_sprites/adventurer-attack1-02.png'), pygame.image.load('images/character_sprites/adventurer-attack1-03.png'), pygame.image.load('images/character_sprites/adventurer-attack1-04.png'), pygame.image.load('images/character_sprites/adventurer-attack2-00.png'), pygame.image.load('images/character_sprites/adventurer-attack2-01.png'), pygame.image.load('images/character_sprites/adventurer-attack2-02.png'), pygame.image.load('images/character_sprites/adventurer-attack2-03.png'),pygame.image.load('images/character_sprites/adventurer-attack2-04.png'),pygame.image.load('images/character_sprites/adventurer-attack2-05.png'),pygame.image.load('images/character_sprites/adventurer-attack3-00.png'), pygame.image.load('images/character_sprites/adventurer-attack3-01.png'), pygame.image.load('images/character_sprites/adventurer-attack3-02.png'), pygame.image.load('images/character_sprites/adventurer-attack3-03.png'),pygame.image.load('images/character_sprites/adventurer-attack3-04.png'),pygame.image.load('images/character_sprites/adventurer-attack3-05.png')]
+attackLeft = [pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack1-00.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack1-01.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack1-02.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack1-03.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack1-04.png'),True, False), pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack2-00.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack2-01.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack2-02.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack2-03.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack2-04.png'),True, False), pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack2-05.png'), True, False), pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack3-00.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack3-01.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack3-02.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack3-03.png'),True, False),pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack3-04.png'),True, False), pygame.transform.flip(pygame.image.load('images/character_sprites/adventurer-attack3-05.png'), True, False)]
+#airAttack =
+#airAttackLeft
+
 
 clock = pygame.time.Clock()
 class player():
@@ -28,16 +34,19 @@ class player():
         self.y = y
         self.width = width
         self.height = height
-        self.vel = 5
+        self.vel = 10
         self.isJump = False
         self.left = False
         self.right = False
+        self.isIdle = True
         self.jpleft = False
         self.jpright = False
+        self.isAttack = False
+        self.isJump = False
         self.walkCount = 0
         self.idleCount = 0
         self.jumpframeCount = 0
-        self.isJump = False
+        self.attackCount = 0
         self.jumpCount = 10
         
             
@@ -51,11 +60,14 @@ class player():
         if self.jumpframeCount +1 > 9:
             self.jumpframeCount = 0
             
-        if self.left and not self.isJump:
+        if self.attackCount +1 >34:
+            self.attackCount = 0
+            
+        if self.left and not self.isJump and not self.isIdle and not self.isAttack:
             win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
             self.walkCount += 1
             
-        elif self.right and not self.isJump:
+        elif self.right and not self.isJump and not self.isIdle and not self.isAttack:
             win.blit(walkRight[self.walkCount//3], (self.x,self.y))
             self.walkCount += 1
             
@@ -65,59 +77,66 @@ class player():
         elif self.isJump and self.left:
             win.blit(jumpLeft[self.jumpframeCount//3], (self.x,self.y))
             
-        elif self.isJump and not self.right and not self.left:
-            win.blit(jumpRight[self.jumpframeCount//3], (self.x,self.y))
+        #elif self.isJump and not self.right and not self.left:
+        #    win.blit(jumpRight[self.jumpframeCount//3], (self.x,self.y))
             
-        if not self.left and not self.right and not self.isJump:
-            win.blit(char[self.idleCount//5], (self.x,self.y)) 
-            self.idleCount += 1        
+        if self.isIdle and self.right and not self.isJump:
+            win.blit(idle[self.idleCount//5], (self.x,self.y)) 
+            self.idleCount += 1
+            
+        elif self.isIdle and self.left and not self.isJump:
+            win.blit(idleLeft[self.idleCount//5], (self.x,self.y)) 
+            self.idleCount += 1
         
-#char_width = 30
-#char_height = 37
-#x = 0
-#y = SCREEN_HEIGHT-char_height-60
-#char_vel = 10
-
-#left = False
-#right = False
-#jpleft = False
-#jpright = False
-#walkCount = 0
-#idleCount = 0
-#jumpframeCount = 0
-
-
+        elif self.isIdle and not self.left and not self.right and not self.isJump:
+            win.blit(idle[self.idleCount//5], (self.x,self.y)) 
+            self.idleCount += 1
+            
+        if self.isAttack and self.right:
+            win.blit(attack[self.attackCount//2], (self.x,self.y))
+            self.attackCount += 1
+            
+        if self.isAttack and self.left:
+            win.blit(attackLeft[self.attackCount//2], (self.x,self.y))
+            self.attackCount += 1
+    def Idle(self):
+        adventurer.isIdle = True
+        adventurer.walkCount = 0
+        adventurer.isAttack = False
+        self.attackCount = 0
+        
+    def runLeft(self):
+        if adventurer.x > adventurer.vel:
+            adventurer.x -= adventurer.vel
+            adventurer.left = True
+            adventurer.right = False
+            adventurer.isIdle = False
+            adventurer.isAttack = False
+            self.attackCount = 0
+            
+    def runRight (self):
+        if adventurer.x < SCREEN_WIDTH-adventurer.width:
+            adventurer.x += adventurer.vel
+            adventurer.left = False
+            adventurer.right = True
+            adventurer.isIdle = False
+            adventurer.isAttack = False
+            self.attackCount = 0
+            
+    def Jump(self):
+        adventurer.isJump=True
+        adventurer.isIdle = False
+        adventurer.isAttack = False
+        adventurer.walkCount = 0
+        self.attackCount = 0
+        
+    def Attack (self):
+        if adventurer.isJump == False:
+            adventurer.isAttack = True
+            adventurer.isIdle = False
 
 def redrawGameWindow():
-    '''
-    global walkCount
-    global idleCount
-    global jumpframeCount
-    global isJump
-    win.blit(bg, (0,0))
-    
-    if walkCount + 1 > 18: # 3 frames per walking frame
-        walkCount = 0
-    if idleCount +1 > 15: # 5 frames per idle frame
-        idleCount = 0
-    if jumpframeCount +1 > 9:
-        jumpframeCount = 0
-    if left and not isJump:
-        win.blit(walkLeft[walkCount//3], (x,y))
-        walkCount += 1
-    elif right and not isJump:
-        win.blit(walkRight[walkCount//3], (x,y))
-        walkCount += 1
-    if isJump and right:
-        win.blit(jumpRight[jumpframeCount//3], (x,y))
-    elif isJump and left:
-        win.blit(jumpLeft[jumpframeCount//3], (x,y))
-    elif isJump and not right and not left:
-        win.blit(jumpRight[jumpframeCount//3], (x,y))
-    if not left and not right and not isJump:
-        win.blit(char[idleCount//5], (x,y)) 
-        idleCount += 1
-    '''
+
     win.blit(bg, (0,0))
     adventurer.draw(win)
     pygame.display.update()     
@@ -126,37 +145,35 @@ def redrawGameWindow():
 adventurer = player (0, SCREEN_HEIGHT-100, 30, 37)
 run = True
 while run:
-    clock.tick(60)
+    clock.tick(30)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_a] and adventurer.x > adventurer.vel:
-        adventurer.x -= adventurer.vel
-        adventurer.left = True
-        adventurer.right = False
-    elif keys[pygame.K_d] and adventurer.x < SCREEN_WIDTH-adventurer.width:
-        adventurer.x += adventurer.vel
-        adventurer.left = False
-        adventurer.right = True
+    if keys[pygame.K_a]:
+        adventurer.runLeft()
+        
+    elif keys[pygame.K_d]:
+        adventurer.runRight()
+        
+    elif keys [pygame.K_f]:
+        adventurer.Attack()
+
     else:
-        adventurer.right = False
-        adventurer.left = False
-        adventurer.walkCount = 0
+        adventurer.Idle()
         
     if not(adventurer.isJump):
         if keys[pygame.K_SPACE]:
-            adventurer.isJump=True
-            adventurer.right = False
-            adventurer.left = False
-            adventurer.walkCount = 0
+            adventurer.Jump()
     else:
         if adventurer.jumpCount>= -10:
-            adventurer.y -= (adventurer.jumpCount*abs(adventurer.jumpCount))*0.25
+            adventurer.y -= (adventurer.jumpCount*abs(adventurer.jumpCount))*0.35
             adventurer.jumpCount -= 1
         else:
             adventurer.isJump = False
+            adventurer.isAttack = False
+            adventurer.isIdle = True
             adventurer.jumpCount = 10
             
     redrawGameWindow()
