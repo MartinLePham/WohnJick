d8ffcb599b7ec51ad5f5c59e21fe3292cc160e10
RachelW27 <47265266+RachelW27@users.noreply.github.com>
2019-04-30 14:45:49 -0500

    Add files via upload

---
+++ GameGui.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 30 14:19:48 2019
+
+@author: rachelengelbrecht
+"""
+from tkinter import *
+window = Tk()
+def processSTART():
+    return(print('final running file name here'))
+label = Label( window, text="Start Game?")
+button = Button(window, text = "START", command=processSTART)
+
+label.pack() #place button on the window
+button.pack() #place button on the window
+
+window.mainloop() #somewhere inside turtle.done(), this gets called.
+
+
+
