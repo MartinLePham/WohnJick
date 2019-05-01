9241aca3998671264bf39eea7294dd33340ef384
MartinLePham <47228488+MartinLePham@users.noreply.github.com>
2019-05-01 10:22:50 -0500

    Update GameGui.py

--- GameGui.py
+++ GameGui.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 30 14:19:48 2019
-
-@author: rachelengelbrecht
-"""
 from tkinter import *
 window = Tk()
 def processSTART():
