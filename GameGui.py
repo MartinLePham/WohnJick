3f3125d86a03d664901ca6374cac8677461416ad
ap16080 <47228489+ap16080@users.noreply.github.com>
2019-05-09 09:44:15 -0500

    Update GameGui.py

--- GameGui.py
+++ GameGui.py
@@ -1,7 +1,7 @@
 from tkinter import *
 window = Tk()
 def processSTART():
-    return(print('platform-scroller.py'))
+    return(print('platform_scroller.py'))
 label = Label(window, text="Start Game?")
 button = Button(window, text = "START", command=processSTART)
 
